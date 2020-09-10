fada5ba307b18d12d363e696413515db4af90a96
lenablechmann <57761364+lenablechmann@users.noreply.github.com>
2020-09-10 10:44:05 +0200

    Add files via upload

---
+++ ex11star.c
@@ -0,0 +1,28 @@
+//Showing the input of random entries.
+#include <stdio.h>
+
+int main(void)
+{
+
+  int i, j, k;
+
+  i = 1;
+  printf("11a) is %d\n", i++ - 1);
+  printf("11a) final is %d\n", i);
+  /*
+  i = 10, j = 5;
+  printf("11b) is %d\n", i++ - ++j);
+  printf("11b) final is %d and %d\n", i, j);
+
+  i = 7, j = 8;
+  printf("11c) is %d\n", i++ - --j);
+  printf("11c) final is %d and %d\n", i, j);
+
+  i = 3, j = 4, k = 5;
+  printf("11d) is %d\n", i++ - j++ + --k);
+  printf("11d) final is %d and %d and %d\n", i, j, k);
+
+  */
+
+  return 0;
+}
