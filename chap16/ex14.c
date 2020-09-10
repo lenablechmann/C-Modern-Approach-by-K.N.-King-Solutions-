81cd9effb5f6f450ca15535aa70c80d82d8b0603
Lena Blechmann <lenablechmann@gmail.com>
2020-09-10 12:55:41 +0200

    added the area function

--- chap16/ex14.c
+++ chap16/ex14.c
@@ -4,22 +4,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-struct point
-{
-    int x, y;
-};
-struct point center;
-
-struct rectangle
-{
-    struct point upper_left, lower_right;
-};
-struct rectangle the_rectangle;
+#define PI 3.1415926535
 
 struct shape
 {
     int shape_kind;
-    struct point center;
+    struct point
+    {
+        int x, y;
+    } center;
     union
     {
         struct
@@ -33,7 +26,7 @@ struct shape
     } u;
 } s;
 
-int area_of_rectangle(struct rectangle the_rectangle);
+int area_of_shape(struct shape s);
 
 int main(void)
 {
@@ -43,7 +36,7 @@ int main(void)
     double scale_factor;
 
     printf("Whats the center point of your shape is it gonna be (x y)? ");
-    scanf("%d/%d", &s.center.x, &s.center.y);
+    scanf("%d %d", &s.center.x, &s.center.y);
 
     printf("Which shape do you want to work with (0 for circle, 1 for rectangle): ");
     scanf(" %d", &s.shape_kind);
@@ -63,18 +56,27 @@ int main(void)
             return 1;
         }
     }
+    else if (s.shape_kind == 0)
+    {
+        printf("Whats the radius of the circle is it gonna be? ");
+        scanf("%d", &s.u.circle.radius);
+        if (s.u.circle.radius < 0)
+        {
+            printf("These values must be positive, retry.\n");
+            return 1;
+        }
+    }
 
-    printf("The area of your rectangle is %d.\n", area_of_rectangle(the_rectangle));
+    printf("The area of your shape is %d.\n", area_of_shape(s));
 
     return 0;
 }
 
-int area_of_rectangle(struct rectangle the_rectangle)
+int area_of_shape(struct shape s)
 {
-    int width, height;
-
-    width = the_rectangle.lower_right.x - the_rectangle.upper_left.x;
-    height = the_rectangle.upper_left.y - the_rectangle.lower_right.y;
-
-    return (width * height);
+    // 0 is a circle, 1 is a rectangle
+    if (s.shape_kind == 0)
+        return ((int)(s.u.circle.radius * s.u.circle.radius * PI));
+    else
+        return (s.u.rectangle.height * s.u.rectangle.width);
 }
