03ffb80d6293f784930500a07d3fbe559e3d8550
Lena Blechmann <lenablechmann@gmail.com>
2020-09-10 17:18:49 +0200

    Basic task initialization of the ex18.c

---
+++ chap16/ex18.c
@@ -0,0 +1,13 @@
+/*  Describe a chessboard with its pieces via structures and enums:
+    a) define two enum types: color and pieces
+    b) using the aforementioned types define a struct type square that can store em
+    c) using type square declare an 8x8 array that can store the entirety of the chessboard
+    d) initialize the chessboard */
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    return 0;
+}
